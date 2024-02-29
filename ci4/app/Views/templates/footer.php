8855c3542bf0834eea8608c31cfaa5dfb3726664
Gian Sobrevinas <gsobrevinas@student.apc.edu.ph>
2024-02-29 15:29:59 +0800

    upload

---
+++ ci4/app/Views/templates/footer.php
@@ -0,0 +1,6 @@
+<div>
+	<h3>put wave element here</h3>
+	<em>&copy; 2022</em>
+</div>
+</body>
+</html>
---
+++ ci4/app/Views/templates/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport"
+content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>CodeIgniter Tutorial</title>
+<link rel="stylesheet" href="<?php echo base_url('/CSS/header.css');?>">
+</head>
+<body>
+<div class="nav">  
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="./News.php">News</a></li>
+  <li><img src="" width="64px" height="64px" ></li>
+  <li><a href="./Archon.php">Artworks</a></li>
+  <li ><a href="./About.php">About</a></li>
+  <li><a href="./guestbook.php">Update Me!</a></li>
+</ul>
+</div>
